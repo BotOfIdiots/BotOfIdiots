15626c6de431a20fc93b2589f7f45af91a83e377
Patrick Riensta <prive@p-rienstra.nl>
2020-10-07 02:29:39 +0200

    Added Version Command

--- Modules/Commands.cs
+++ Modules/Commands.cs
@@ -1,14 +1,35 @@
-﻿using System.Threading.Tasks;
+using System;
+using System.Net.Sockets;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
+using DiscordBot.Modules;
 
 namespace DiscordBot.Modules
 {
     public class Commands : ModuleBase<SocketCommandContext>
     {
         [Command("ping")]
-        public async Task ping()
+        public async Task Ping()
         {
             await ReplyAsync("Pong");
+        }
+
+        [Command("version")]
+        public async Task Version()
+        {
+            Embed embed = new EmbedBuilder
+            {
+                Title = "Version: " + Program.Version(),
+            }
+                .WithAuthor(Context.Client.CurrentUser)
+                .WithFooter(Program.Version())
+                .WithCurrentTimestamp()
+                .Build();
+            
+            await ReplyAsync(embed: embed);
         }
     }
 }
