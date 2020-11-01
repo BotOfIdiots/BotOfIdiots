3295f1cf2eb59fbf6e293b409eaa373a2df50bc6
Patrick Riensta <prive@p-rienstra.nl>
2020-11-01 15:20:51 +0100

    Fixed merge conflict and did some optimization

--- Modules/ViolationManager.cs
+++ Modules/ViolationManager.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using DiscordBot.Models;
 using LiteDB;
-
 
 namespace DiscordBot.Modules
 
@@ -12,26 +13,20 @@ namespace DiscordBot.Modules
     public static class ViolationManager
     {
         /// <summary>
-        /// 
+        /// Create a New violation insert it into the database and return an embed
         /// </summary>
         /// <param name="violationType"> Violation type. 1 = Ban, 2 = Kick, 3 = mute, 4 = warn </param>
         /// <param name="violator">User that committed the violation</param>
         /// <param name="reason">Reason for the violation</param>
         /// <param name="context">Command Context</param>
         /// <returns>Embed</returns>
-        public static Embed NewViolation(SocketGuildUser violator, string reason, SocketCommandContext context, int violationType = 0)
+        public static Embed NewViolation(SocketGuildUser violator, string reason, SocketCommandContext context,
+            int violationType = 0)
+
         {
             try
             {
                 DateTime date = DateTime.Now;
-
-//                string violationQuery =
-//                    "INSERT INTO Violations (UserID, ViolationDate, ViolationType, Reason) VALUES (@0, @1, @2, @3)";
-//                SqlHandler.NewViolationQuery(violationQuery, user.Id, date, violationType, reason);
-//
-//                string violationIDQuery = "SELECT ViolationID FROM Violations WHERE ViolationDate = @0";
-//                List<object> result = SqlHandler.SelectQuery(violationIDQuery, date);
-//                string violationId = result[0].ToString();
 
                 Violation violation = new Violation
                 {
@@ -41,14 +36,15 @@ namespace DiscordBot.Modules
                     Reason = reason,
                     Date = date
                 };
-                
+
+
                 InsertViolation(violation);
-                
+
                 violation = GetCreatedRecord(date);
-                
-                Embed violationEmbed = ViolationEmbed(violation, context);
-                return violationEmbed;
+
+                return ViolationEmbed(violation, context);
             }
+
             catch (IndexOutOfRangeException)
             {
                 Embed error = new EmbedBuilder
@@ -56,11 +52,12 @@ namespace DiscordBot.Modules
                         Title = "Te weinig argumenten"
                     }
                     .WithDescription("Commando is uitgevoerd met te weinig argumenten")
-                    .AddField("Example", DiscordBot.Config["CommandPrefix"]+"ban [user] {reason}")
+                    .AddField("Example", DiscordBot.Config["CommandPrefix"] + "ban [user] {reason}")
                     .Build();
-                
+
                 return error;
             }
+
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
@@ -76,14 +73,51 @@ namespace DiscordBot.Modules
             }
         }
 
-//        public static string ViolationCount(ulong userId)
-//        {
-//            string getCountQuery = "SELECT COUNT(ViolationID) FROM Violations WHERE UserID = @0";
-//            List<object> result = SqlHandler.SelectQuery(getCountQuery, userId);
-//            string violationCount = result[0].ToString();
-//            return violationCount;
-//        }
+        
+        /// <summary>
+        /// Create a List of Violations commited by user
+        /// </summary>
+        /// <param name="user">The user of which to return the violations</param>
+        /// <returns>List<Violations></returns>
+        public static List<Violation> GetViolations(ulong user)
+        {
+            List<Violation> violations = new List<Violation>();
 
+            using (var db = new LiteDatabase(DiscordBot.Config + "Database.db"))
+            {
+                var table = db.GetCollection<Violation>("violations");
+                IEnumerable<Violation> queryData = table.Find(x => x.UserId == user);
+
+                foreach (Violation record in queryData)
+                {
+                    violations.Add(record);
+                }
+            }
+
+            return violations;
+        }
+
+        
+        /// <summary>
+        /// Get a specific violation
+        /// </summary>
+        /// <param name="id">Id of violation to return</param>
+        /// <param name="context">Context of issued command</param>
+        /// <returns></returns>
+        public static Embed GetViolation(int id, SocketCommandContext context)
+        {
+            Violation violation = GetViolationRecord(id);
+
+            return ViolationEmbed(violation, context);
+        }
+
+        
+        /// <summary>
+        /// This functions returns an embed with the information of the committed violation
+        /// </summary>
+        /// <param name="violation">The violation information</param>
+        /// <param name="context">The context of the command that wants the Embed</param>
+        /// <returns></returns>
         private static Embed ViolationEmbed(Violation violation, SocketCommandContext context)
         {
             String violationTitle;
@@ -98,7 +132,7 @@ namespace DiscordBot.Modules
                 case 3:
                     violationTitle = "Muted";
                     break;
-                case "4":
+                case 4:
                     violationTitle = "Unmuted";
                     break;
                 default:
@@ -108,12 +142,11 @@ namespace DiscordBot.Modules
 
             Embed embed = new EmbedBuilder
                 {
-                    
                     Title = violationTitle,
                     Color = Color.Red
                 }
                 .WithAuthor(context.Client.CurrentUser)
-                .AddField("User:", "<@!"+ violation.UserId+">", true)
+                .AddField("User:", "<@!" + violation.UserId + ">", true)
                 .AddField("Date:", DateTime.Now, true)
                 .AddField("Moderator:", context.User.Mention)
                 .AddField("Reason:", violation.Reason)
@@ -124,8 +157,13 @@ namespace DiscordBot.Modules
 
             return embed;
         }
-        
-        public static void InsertViolation(Violation record) {
+
+        /// <summary>
+        /// Insert a Violation object into the database
+        /// </summary>
+        /// <param name="record">The object to insert</param>
+        public static void InsertViolation(Violation record)
+        {
             using (var db = new LiteDatabase(DiscordBot.Config + "Database.db"))
             {
                 var table = db.GetCollection<Violation>("violations");
@@ -143,7 +181,14 @@ namespace DiscordBot.Modules
 //            }
 //        }
 //        
-        public static Violation GetCreatedRecord(DateTime date) {
+
+        /// <summary>
+        /// Return a Database record based on the specified date
+        /// </summary>
+        /// <param name="date"> Date of created record to return</param>
+        /// <returns></returns>
+        public static Violation GetCreatedRecord(DateTime date)
+        {
             using (var db = new LiteDatabase(DiscordBot.Config + "Database.db"))
             {
                 var table = db.GetCollection<Violation>("violations");
@@ -151,8 +196,14 @@ namespace DiscordBot.Modules
                 return table.FindOne(x => x.Date == date);
             }
         }
-        
-        public static Violation GetViolationRecord(int id) {
+
+        /// <summary>
+        /// Return a Database record bases on a specified Identifier
+        /// </summary>
+        /// <param name="id">identifier of the record to return</param>
+        /// <returns></returns>
+        public static Violation GetViolationRecord(int id)
+        {
             using (var db = new LiteDatabase(DiscordBot.Config + "Database.db"))
             {
                 var table = db.GetCollection<Violation>("violations");
@@ -160,7 +211,5 @@ namespace DiscordBot.Modules
                 return table.FindOne(x => x.Id == id);
             }
         }
-        
-        
     }
 }
